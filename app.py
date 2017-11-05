86c185f5de8e91271bc16b9d8560f084545ba737
Emily Strong <erstrong.06@gmail.com>
2017-11-04 23:27:56 -0400

    modifications

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ from flask_restful import Resource, Api
 import MySQLdb as sql
 from json import dumps
 from flask import jsonify
-
+import configparser
 
 app = Flask(__name__)
 api = Api(app)
 
+
+
+config=configparser.RawConfigParser()
+configFilePath=r'config.py'
+config.read(configFilePath)
+MYSQL_DATABASE_HOST=config.get('app','MYSQL_DATABASE_HOST');
+MYSQL_DATABASE_USER=config.get('app','MYSQL_DATABASE_USER');
+MYSQL_DATABASE_PASSWORD=config.get('app','MYSQL_DATABASE_PASSWORD');
+
+#app.config.from_pyfile('config.py')
 #con = sql.connect(host = "127.0.0.1", user = "root", passwd = "", db = "properties")
-con = sql.connect(host = MYSQL_DATABASE_HOST, user = MYSQL_DATABASE_USER, passwd = MYSQL_DATABASE_PASSWORD, db = "properties")
+con = sql.connect(host = MYSQL_DATABASE_HOST, user = MYSQL_DATABASE_USER, passwd = MYSQL_DATABASE_PASSWORD, db = "adsesrk")
 # MySQL configurations
 #app.config['MYSQL_DATABASE_USER'] = 'root'
 #app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -27,7 +37,7 @@ def get_neighbors():
     cur = con.cursor()
     with con:
         cur.execute(query)
-        cur.execute("select *,3956*2*asin(sqrt(power(sin((@orig_lat - abs(dest.latitude)) * pi()/180/2),2) + cos(@orig_lat * pi()/180) * cos(abs(dest.latitude) * pi()/180) * power(sin((@orig_lon - dest.longitude) * pi()/180/2),2))) as distance from properties.properties dest order by distance limit 10;")
+        cur.execute("select *,3956*2*asin(sqrt(power(sin((@orig_lat - abs(dest.latitude)) * pi()/180/2),2) + cos(@orig_lat * pi()/180) * cos(abs(dest.latitude) * pi()/180) * power(sin((@orig_lon - dest.longitude) * pi()/180/2),2))) as distance from adsesrk.properties dest order by distance limit 10;")
     return jsonify(data=cur.fetchall())
 
 
